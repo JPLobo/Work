f670901e06cd038e0e24b6d25b37713943e480b9
John <Johnplobocki@gmail.com>
2018-09-25 08:29:56 -0500

    bones2

--- src/InputExamples/bones.java
+++ src/InputExamples/bones.java
@@ -9,6 +9,7 @@ Scanner input = new Scanner(System.in);
 System.out.println("How many bones have you broken?");
  bones = input.nextInt();
  System.out.println("Number of bones is:" + bones);
+ input.close();
 	}
 
 }
