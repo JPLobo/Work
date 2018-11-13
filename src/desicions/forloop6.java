3a9c1b3ee6f3de57d594a43b3673e7a6dda4d4a5
John <Johnplobocki@gmail.com>
2018-11-13 08:38:36 -0600

    forloopsums

---
+++ src/desicions/forloop6.java
@@ -0,0 +1,17 @@
+package desicions;
+
+
+
+public class forloop6 {public static void main(String[] args) {
+int x = 0;
+int total = 0;
+for (x = 1; x <= 1500; x++) {
+	total = total + x;
+}
+	System.out.println(total);
+	
+}
+
+	
+
+}
