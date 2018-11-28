0e1133418882b5caaeb796e9279dc2b7a19d603e
John <Johnplobocki@gmail.com>
2018-11-28 08:53:28 -0600

    charAt

--- src/strings/Failexample.java
+++ src/strings/Failexample.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Failexample {
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
+	System.out.println("What is your age");
+		int age = input.nextInt();
+		input.nextLine();
 			
 	
 	}
