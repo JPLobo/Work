7cc6e258f5830041b47d02b362fa4084d183ab4b
John <Johnplobocki@gmail.com>
2018-11-29 08:57:46 -0600

    toLowerCase

---
+++ src/strings/Stringslowercase.java
@@ -0,0 +1,15 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Stringslowercase {public static void main(String [] args){
+
+	Scanner input = new Scanner(System.in);
+System.out.println("Enter a sentence");
+String a = input.nextLine();
+System.out.println(a.toLowerCase());
+System.out.println(a);
+
+
+}
+}
