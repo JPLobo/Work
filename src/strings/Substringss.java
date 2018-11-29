52c2d547a2dc2f4658b7519c24f602927ebf9d3f
John <Johnplobocki@gmail.com>
2018-11-29 08:52:50 -0600

    substring

---
+++ src/strings/Substringss.java
@@ -0,0 +1,12 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Substringss {public static void main(String [] args){
+
+	Scanner input = new Scanner(System.in);
+System.out.println("Enter a sentence");
+String a = input.nextLine();
+System.out.println(a.substring(3, 5));
+}
+}
