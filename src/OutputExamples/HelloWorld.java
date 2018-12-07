95933e1e063623300a888fac6827110ff3aaaea6
John <Johnplobocki@gmail.com>
2018-12-07 08:39:55 -0600

    heelowordl

--- src/OutputExamples/HelloWorld.java
+++ src/OutputExamples/HelloWorld.java
@@ -4,10 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-final int MAXSTUDENS = 30;
-final char MIDDLEINITIAL = 'p';
-int numStudents = 0;
-System.out.println (MAXSTUDENS);
+
+System.out.println ("Helloworld");
 	}
 
 }
