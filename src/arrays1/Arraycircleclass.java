38a3b13b7b8aa95b0ad883747e8adf92c8b24aad
John <Johnplobocki@gmail.com>
2018-12-10 09:05:17 -0600

    unfinishedarray

---
+++ src/arrays1/Arraycircleclass.java
@@ -0,0 +1,22 @@
+package arrays1;
+
+import java.util.Scanner;
+import java.util.Random;
+import objectorientedprogram.Rectangle;
+public class Arraycircleclass {
+	public static void main(String [] args){
+		Rectangle bob = new Rectangle();
+		String [] rectangles = new String [100];
+	Random j = new Random();
+int max = 56;
+int min = 10;
+
+	Scanner input = new Scanner(System.in);
+	for (int x = 0; x< rectangles.length; x++ ) {
+	
+
+	}
+	
+	
+}
+}
