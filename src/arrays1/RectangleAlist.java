975335738f66a66a402d8f053bfb87ad1eecbd53
John <Johnplobocki@gmail.com>
2019-01-08 08:20:28 -0600

    Rectangle;List

---
+++ src/arrays1/RectangleAlist.java
@@ -0,0 +1,35 @@
+package arrays1;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import objectorientedprogram.Rectangle;
+
+public class RectangleAlist {
+	public static void main(String[] args) {
+		int min =50;
+		int max = 70;
+		int num = 0;
+		int num2 = 0;
+		Random x = new Random ();
+		
+		Scanner input = new Scanner(System.in);
+		
+ArrayList <Rectangle> recs = new ArrayList<Rectangle>();
+for (int j = 0; j< recs.size(); j++) {
+	int width =  min + x.nextInt(max-min + 1);
+	 int length=min + x.nextInt(max-min + 1);
+	recs.add(new Rectangle(width, length));
+	
+}
+for( int g =recs.size(); g>0; g-- ) {
+	Rectangle are = recs.get(g);
+	System.out.println(are.area());
+}
+
+
+
+}
+}
