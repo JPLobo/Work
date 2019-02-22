cbbb1625d9101779364ab253f6e38baf7713c67f
John <Johnplobocki@gmail.com>
2019-02-22 08:11:24 -0600

    Binarydescendingorder

---
+++ src/objectorientedprogram/BinaryDescendingorder.java
@@ -0,0 +1,22 @@
+package objectorientedprogram;
+
+public class BinaryDescendingorder {
+	public static int binarySearchDescending(int [] temp, int value){
+		int low = 0;
+		int high = temp.length - 1;
+		while (low <= high){
+			int mid = (low + high) / 2;
+			if(temp[mid] == value)
+				return mid;
+			else
+				if(temp[mid] > value)
+					low = mid + 1;
+				else
+					high = mid - 1;
+		}
+		return -1;
+	}
+	
+}
+
+
