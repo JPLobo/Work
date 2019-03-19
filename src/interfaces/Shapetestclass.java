063ffc6d49086d4164d1110daa7e4d0f49aceac6
John <Johnplobocki@gmail.com>
2019-03-19 09:00:28 -0500

    bothobjects

--- src/interfaces/Shapetestclass.java
+++ src/interfaces/Shapetestclass.java
@@ -6,6 +6,7 @@ public class Shapetestclass {
 	public static void main (String[]args) {
 		double le = 0;
 		double wi = 0;
+		double ra = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is the length of the Rectangle?");
 		le = input.nextDouble();
@@ -14,6 +15,11 @@ public class Shapetestclass {
 		Shape bob = new Rect(le, wi);
 		System.out.println(bob.area());
 		System.out.println(bob.perimeter());
+		System.out.println("What is the radius of the Circle");
+		ra = input.nextDouble();
+		Shape sue = new Circlet(ra);
+		System.out.println(sue.area());
+		System.out.println(sue.perimeter());
 	}
 
 }
