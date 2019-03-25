07eff43b1fcc526c52cabac8b77c62b24c54643c
John <Johnplobocki@gmail.com>
2019-03-25 08:37:20 -0500

    Animalclass

--- src/interfaces/Cat.java
+++ src/interfaces/Cat.java
@@ -4,9 +4,14 @@ public class Cat implements Animal {
 private int age;
 private String name;
 private boolean friendly;
+public Cat() {
+	name = "Dumb";
+	age = 6;
+	friendly = false;
+}
 
 public void act() {
-age =+ 9;
+age += 9;
 if (friendly == false) {
 	friendly = true;
 	}
