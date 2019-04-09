d4d6b62685531bf4b18281fb009e3b096706c979
John <Johnplobocki@gmail.com>
2019-04-09 08:16:11 -0500

    Downarrow

--- src/inheritance/IRectangle.java
+++ src/inheritance/IRectangle.java
@@ -1,5 +1,20 @@
 package inheritance;
 
-public class IRectangle {
+public class IRectangle extends Quad{
 
+public IRectangle () {
+	setside1(5);
+	setside2(10);
+	setside3(5);
+	setside4(10);
+}
+public IRectangle (double l, double w) {
+	setside1(l);
+	setside2(w);
+	setside3(l);
+	setside4(w);
+}
+public double Area() {
+	return getside1() * getside2();
+}
 }
