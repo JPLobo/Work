8f1be14f4d4b42185ab237febc7089fe602fafc6
John <Johnplobocki@gmail.com>
2019-04-11 08:41:07 -0500

    Basketball

--- src/inheritance/Ball.java
+++ src/inheritance/Ball.java
@@ -12,4 +12,7 @@ return "Ball method1";
 public String method2(){
 return "Ball method2";
 }
+public String method4(){
+return "Ball method4";
+}
 }
---
+++ src/inheritance/Ballsubclass.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Ballsubclass {
+public static void main(String[] args) {
+	Basketball bob = new Basketball();
+	System.out.println(bob.method1());
+	System.out.println(bob.method2());
+	System.out.println(bob.method3());
+	System.out.println(bob.method4());
+	Ball sue = new Basketball();
+	
+}
+}
