a84733adb6d68cd2f8bd1aa5ccd7103e8ddaa615
John <Johnplobocki@gmail.com>
2019-04-24 08:43:29 -0500

    apreveiwfr1

---
+++ src/inheritance/Screen.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+import java.util.ArrayList;
+
+public class Screen {
+	
+		private ArrayList<int>[] data;
+		private int numCols;
+		
+		public Screen (int width, int height) {
+		numCols = width;
+		data.add(height);
+		}
+		public Apreviewfr1 pixelAt (int row, int col)
+		{ 
+			
+		}
+		
+		public void pixelOn (int row, int col) {
+
+		}
+}
